24f8bce25734092e9a2977ba4cba6a9cef5c5048
Shreenidhi Bharadwaj H K <shreenidhihk@outlook.com>
2022-01-07 17:03:44 +0530

    Updated the JenkinsFile

--- test-outputs.py
+++ test-outputs.py
@@ -1,14 +1,23 @@
 import filecmp
 class TestGroovyTemplates:
     def testGroovySuccess(self):
-        f1 = "templates/resources/sample-groovy-new.result"
-        f2 = "success-email.result"
-        # shallow comparison
-        result = filecmp.cmp(f1, f2)
-        print(result)
-        # deep comparison
-        result = filecmp.cmp(f1, f2, shallow=False)
-        print(result)   
+        f1 = open("templates/resources/sample-groovy-new.result")
+        f2 = open("success-email.result")
+        i = 0
+        for line1 in f1:
+            i += 1
+            for line2 in f2:           
+                if line1 == line2:  
+                    print("Line ", i, ": IDENTICAL")       
+                else:
+                    print("Line ", i, ":")
+                    print("\tFile 1:", line1, end='')
+                    print("\tFile 2:", line2, end='')
+                break
+        
+        # closing files
+        f1.close()                                       
+        f2.close()    
     def testGroovyFailure(self):
         f1 = "templates/resources/sample-groovy-new-fail.result"
         f2 = "failure-email.result"
@@ -26,4 +35,4 @@ if __name__ == '__main__':
     test = TestGroovyTemplates()
     test.testGroovySuccess()
     test.testGroovyFailure()
-self
+ 
