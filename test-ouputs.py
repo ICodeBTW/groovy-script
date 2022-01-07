6fc51f6d526acdac055adc1b2e2fb0152fa43602
Shreenidhi Bharadwaj H K <shreenidhihk@outlook.com>
2022-01-07 16:52:49 +0530

    Updated the JenkinsFile

---
+++ test-ouputs.py
@@ -0,0 +1,28 @@
+import filecmp
+class TestGroovyTemplates:
+    def testGroovySuccess():
+        f1 = "templates/resources/sample-groovy-new.result"
+        f2 = "success-email.result"
+        # shallow comparison
+        result = filecmp.cmp(f1, f2)
+        print(result)
+        # deep comparison
+        result = filecmp.cmp(f1, f2, shallow=False)
+        print(result)   
+    def testGroovyFailure():
+        f1 = "templates/resources/sample-groovy-new-fail.result"
+        f2 = "failure-email.result"
+        # shallow comparison
+        result = filecmp.cmp(f1, f2)
+        print(result)
+        # deep comparison
+        result = filecmp.cmp(f1, f2, shallow=False)
+        print(result)      
+
+
+
+
+if __name__ == '__main__':
+    test = TestGroovyTemplates()
+    test.testGroovySuccess()
+    test.testGroovyFailure()
